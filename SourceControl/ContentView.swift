609b3ed9935b65fa2700edbd303e735ec7b56a97
Didier <did35@yahoo.com>
2024-05-06 16:31:19 +0200

    [Clean] Add commit message documentation

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -5,6 +5,25 @@
 //  Created by Didier Delhaisse on 03/05/2024.
 //
 
+/*
+ COMMIT MESSAGES
+ 
+ NEW FEATURE:
+ [Feature] Description of the feature
+ 
+ BUG IN PRODUCTION:
+ [Patch] Description of patch
+ 
+ BUG NOT IN PRODUCTION:
+ [Bug] Description of the bug
+ 
+ MUNDANE TASK:
+ [Clean] Description of changes
+ 
+ RELEASE:
+ [Release] Description of release
+ */
+
 import SwiftUI
 
 struct ContentView: View {
