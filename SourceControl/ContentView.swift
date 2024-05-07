6b99f995370017bbfb21351c10fe487556dfc942
Didier <did35@yahoo.com>
2024-05-07 07:30:41 +0200

    [Feature] Rename subscribe button

--- SourceControl/ContentView.swift
+++ SourceControl/ContentView.swift
@@ -46,7 +46,7 @@ struct ContentView: View {
                 Text("Source Control!")
                     .foregroundStyle(.white)
                 
-                Button("Click me") {
+                Button("Subscribe") {
                     // To do
                 }
                 .font(.title2)
